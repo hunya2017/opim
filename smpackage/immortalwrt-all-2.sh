5cf0d753b9b3e3b5ff1382a633a8e31aef97e256
hunya2017 <hunya2017@gmail.com>
2025-04-10 16:07:31 +0800

    666

--- smpackage/immortalwrt-all-2.sh
+++ smpackage/immortalwrt-all-2.sh
@@ -103,157 +103,69 @@
 
 
 # 自定义定制选项
-NET="package/base-files/files/bin/config_generate"
-ZZZ="package/emortal/default-settings/files/99-default-settings"
+# NET="package/base-files/files/bin/config_generate"
+# ZZZ="package/emortal/default-settings/files/99-default-settings"
+UCI_DEFAULTS="package/emortal/default-settings/files/etc/uci-defaults/99-custom-init"
 
-#
-sed -i "s#192.168.1.1#192.168.10.1#g" $NET                                                     # 定制默认IP
-# sed -i "s#ImmortalWrt#ImmortalWrt-X86#g" $NET                                          # 修改默认名称为 ImmortalWrt-X86
-# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' $ZZZ                                          # 取消系统默认密码
-echo "uci set luci.main.mediaurlbase=/luci-static/argon" >> $ZZZ                      # 设置默认主题(如果编译可会自动修改默认主题的，有可能会失效)
+# 定义路径变量
+UCI_DEFAULTS="package/emortal/default-settings/files/etc/uci-defaults/99-custom-init"
 
-# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
+# 创建目录
+mkdir -p "$(dirname "$UCI_DEFAULTS")"
 
-# BUILDTIME=$(TZ=UTC-8 date "+%Y.%m.%d") && sed -i "s/\(_('Firmware Version'), *\)/\1 ('ONE build $BUILDTIME @ ') + /" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js              # 增加自己个性名称
-# sed -i "s@list listen_https@# list listen_https@g" package/network/services/uhttpd/files/uhttpd.config               # 停止监听443端口
-# sed -i '/exit 0/i\ethtool -s eth0 speed 2500 duplex full' package/base-files/files//etc/rc.local               # 强制显示2500M和全双工（默认PVE下VirtIO不识别） ImmortalWrt固件内不显示端口状态，可以关闭
+# 写入首次启动脚本内容
+cat << 'EOF' > "$UCI_DEFAULTS"
+#!/bin/sh
 
-# ●●●●●●●●●●●●●●●●●●●●●●●●定制部分●●●●●●●●●●●●●●●●●●●●●●●● #
+# 日志重定向
+exec >/tmp/setup.log 2>&1
 
+# 设置 root 密码
+root_password="password"
+[ -n "$root_password" ] && (echo "$root_password"; sleep 1; echo "$root_password") | passwd > /dev/null
 
-# ================ 网络设置 =======================================
+# 设置 LAN IP
+lan_ip_address="192.168.10.1"
+[ -n "$lan_ip_address" ] && {
+  uci set network.lan.ipaddr="$lan_ip_address"
+  uci commit network
+}
 
-cat >> $ZZZ <<-EOF
-# # 设置网络-旁路由模式
-# uci set network.lan.gateway='192.168.89.248'                     # 旁路由设置 IPv4 网关
-# uci set network.lan.dns='223.5.5.5 119.29.29.29'            # 旁路由设置 DNS(多个DNS要用空格分开)
-# uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
-# uci delete network.lan.type                                  # 旁路由桥接模式-禁用
-# uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
-# uci set dhcp.@dnsmasq[0].filter_aaaa='0'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+# 设置 DHCP 租期
+uci set dhcp.lan.leasetime='2m'
 
-# # 设置防火墙-旁路由模式
-# uci set firewall.@defaults[0].syn_flood='0'                  # 禁用 SYN-flood 防御
-# uci set firewall.@defaults[0].flow_offloading='0'           # 禁用基于软件的NAT分载
-# uci set firewall.@defaults[0].flow_offloading_hw='0'       # 禁用基于硬件的NAT分载
-# uci set firewall.@defaults[0].fullcone='0'                   # 禁用 FullCone NAT
-# uci set firewall.@defaults[0].fullcone6='0'                  # 禁用 FullCone NAT6
-# uci set firewall.@zone[0].masq='1'                             # 启用LAN口 IP 动态伪装
+# 添加静态 DHCP 租约
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac='6A:CE:3B:D4:CB:8B'
+uci set dhcp.@host[-1].ip='192.168.10.158'
 
-# # 旁路IPV6需要全部禁用
-# uci del network.lan.ip6assign                                 # IPV6分配长度-禁用
-# uci del dhcp.lan.ra                                             # 路由通告服务-禁用
-# uci del dhcp.lan.dhcpv6                                        # DHCPv6 服务-禁用
-# uci del dhcp.lan.ra_management                               # DHCPv6 模式-禁用
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac='30:9C:23:E1:E3:72'
+uci set dhcp.@host[-1].ip='192.168.10.107'
 
-# # 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
-# uci set network.ipv6=interface
-# uci set network.ipv6.proto='dhcpv6'
-# uci set network.ipv6.ifname='@lan'
-# uci set network.ipv6.reqaddress='try'
-# uci set network.ipv6.reqprefix='auto'
-# uci set firewall.@zone[0].network='lan ipv6'
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac='7C:2B:E1:13:6E:83'
+uci set dhcp.@host[-1].ip='192.168.10.180'
 
-# uci commit dhcp
-# uci commit network
-# uci commit firewall
+uci commit dhcp
 
-# EOF
+# 配置 zerotier
+uci set zerotier.sample_config.enabled='1'
+uci del zerotier.sample_config.join
+uci add_list zerotier.sample_config.join='d3ecf5726da3eeac'
+uci set zerotier.sample_config.nat='1'
+uci commit zerotier
 
-# =======================================================
+# 配置 vlmcsd 服务
+uci set vlmcsd.config.enabled='1'
+uci set vlmcsd.config.auto_activate='1'
+uci set vlmcsd.config.internet_access='1'
+uci commit vlmcsd
 
-# 修改退出命令到最后
-# cd $HOME && sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
+echo "All done!"
+exit 0
+EOF
 
-# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
+# 添加执行权限
+chmod +x "$UCI_DEFAULTS"
 
-
-
-
-
-# 添加自定义软件包
-
-echo "
-
-# 额外组件
-# CONFIG_GRUB_IMAGES=y
-# CONFIG_VMDK_IMAGES=y
-
-# CONFIG_TARGET_ROOTFS_EXT4FS=y
-# CONFIG_TARGET_EXT4_RESERVED_PCT=0
-# CONFIG_TARGET_EXT4_BLOCKSIZE_4K=y
-# # CONFIG_TARGET_EXT4_BLOCKSIZE_2K is not set
-# # CONFIG_TARGET_EXT4_BLOCKSIZE_1K is not set
-# CONFIG_TARGET_EXT4_BLOCKSIZE=4096
-# # CONFIG_TARGET_EXT4_JOURNAL is not set
-
-
-
-
-# 固件大小
-CONFIG_TARGET_KERNEL_PARTSIZE=16
-CONFIG_TARGET_ROOTFS_PARTSIZE=600
-
-# # Themes
-CONFIG_PACKAGE_luci-theme-argon=y
-
-
-# adguardhome
-CONFIG_PACKAGE_luci-app-adguardhome=y
-
-
-# # arpbind
-CONFIG_PACKAGE_luci-app-arpbind=n
-
-
-# 自动重启
-CONFIG_PACKAGE_luci-app-autoreboot=y
-
-
-# mosdns
-# CONFIG_PACKAGE_luci-app-mosdns=y
-
-
-# netspeedtest chmod +x /etc/init.d/netspeedtest
-CONFIG_PACKAGE_luci-app-netspeedtest=y
-
-
-# nlbwmon
-# CONFIG_PACKAGE_luci-app-nlbwmon=y
-
-
-# openclash
-CONFIG_PACKAGE_luci-app-openclash=y
-
-
-# 关机
-# CONFIG_PACKAGE_luci-app-poweroff=y
-
-
-# passwall
-CONFIG_PACKAGE_luci-app-passwall=y
-
-
-CONFIG_PACKAGE_luci-app-passwall2=n
-
-
-# quickstart
-CONFIG_PACKAGE_luci-app-quickstart=y
-
-
-# store
-CONFIG_PACKAGE_luci-app-store=y
-
-
-# luci-app-ttyd=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-
-
-
-# luci-app-webadmin=y
-CONFIG_PACKAGE_luci-app-webadmin=y
-
-
-
-
-" >> .config
