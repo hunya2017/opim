3f4e1cbbce1e035993cb6e0db2f5ea0aefceab5a
hunya2017 <hunya2017@gmail.com>
2025-04-12 11:33:59 +0800

    112

--- smpackage/immortalwrt-all-2.sh
+++ smpackage/immortalwrt-all-2.sh
@@ -16,57 +16,69 @@
 # 定义路径变量
 UCI_DEFAULTS="package/emortal/default-settings/files/99-default-settings"
 
-# 确保文件存在
+# 确保目录和文件存在
+mkdir -p "$(dirname "$UCI_DEFAULTS")"
 if [ ! -f "$UCI_DEFAULTS" ]; then
   echo "#!/bin/sh" >"$UCI_DEFAULTS"
   echo "exit 0" >>"$UCI_DEFAULTS"
 fi
 
-# 在 exit 0 之前插入内容
-sed -i "/exit 0/i\
-# 日志重定向\n\
-exec >/tmp/setup.log 2>&1\n\
-\n\
-# 设置 root 密码\n\
-echo \"password\" | passwd --stdin root > /dev/null\n\
-\n\
-# 设置 LAN IP\n\
-uci set network.lan.ipaddr=\"192.168.10.1\"\n\
-uci commit network\n\
+# 使用 cat <<EOF 在 exit 0 之前插入内容
+TMP_FILE=$(mktemp)
+cat <<EOF >"$TMP_FILE"
+# 日志重定向
+# exec >/tmp/setup.log 2>&1
 
-\n\
-# 设置 DHCP 租期\n\
-uci set dhcp.lan.leasetime=\"2m\"\n\
-\n\
-# 添加静态 DHCP 租约\n\
-uci add dhcp host\n\
-uci add_list dhcp.@host[-1].mac=\"6A:CE:3B:D4:CB:8B\"\n\
-uci set dhcp.@host[-1].ip=\"192.168.10.158\"\n\
-\n\
-uci add dhcp host\n\
-uci add_list dhcp.@host[-1].mac=\"30:9C:23:E1:E3:72\"\n\
-uci set dhcp.@host[-1].ip=\"192.168.10.107\"\n\
-\n\
-uci add dhcp host\n\
-uci add_list dhcp.@host[-1].mac=\"7C:2B:E1:13:6E:83\"\n\
-uci set dhcp.@host[-1].ip=\"192.168.10.180\"\n\
-\n\
-uci commit dhcp\n\
-\n\
-# 配置 zerotier\n\
-uci set zerotier.sample_config.enabled=\"1\"\n\
-uci del zerotier.sample_config.join\n\
-uci add_list zerotier.sample_config.join=\"d3ecf5726da3eeac\"\n\
-uci set zerotier.sample_config.nat=\"1\"\n\
-uci commit zerotier\n\
-\n\
-# 配置 vlmcsd 服务\n\
-uci set vlmcsd.config.enabled=\"1\"\n\
-uci set vlmcsd.config.auto_activate=\"1\"\n\
-uci set vlmcsd.config.internet_access=\"1\"\n\
-uci commit vlmcsd\n\
-\n\
-echo \"All done!\"" "$UCI_DEFAULTS"
+# 设置 root 密码
+echo "Setting root password..."
+echo "root:password" | chpasswd
+
+# 设置 LAN IP
+echo "Configuring LAN IP..."
+uci set network.lan.ipaddr="192.168.10.1"
+uci commit network
+
+# 设置 DHCP 租期
+echo "Configuring DHCP lease time..."
+uci set dhcp.lan.leasetime="2m"
+
+# 添加静态 DHCP 租约
+echo "Adding static DHCP leases..."
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac="6A:CE:3B:D4:CB:8B"
+uci set dhcp.@host[-1].ip="192.168.10.158"
+
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac="30:9C:23:E1:E3:72"
+uci set dhcp.@host[-1].ip="192.168.10.107"
+
+uci add dhcp host
+uci add_list dhcp.@host[-1].mac="7C:2B:E1:13:6E:83"
+uci set dhcp.@host[-1].ip="192.168.10.180"
+
+uci commit dhcp
+
+# 配置 zerotier
+echo "Configuring ZeroTier..."
+uci set zerotier.sample_config.enabled="1"
+uci del zerotier.sample_config.join
+uci add_list zerotier.sample_config.join="d3ecf5726da3eeac"
+uci set zerotier.sample_config.nat="1"
+uci commit zerotier
+
+# 配置 vlmcsd 服务
+echo "Configuring Vlmcsd service..."
+uci set vlmcsd.config.enabled="1"
+uci set vlmcsd.config.auto_activate="1"
+uci set vlmcsd.config.internet_access="1"
+uci commit vlmcsd
+
+echo "All done!"
+EOF
+
+# 将内容插入到 exit 0 之前
+sed -i "/exit 0/r $TMP_FILE" "$UCI_DEFAULTS"
+rm -f "$TMP_FILE"
 
 # 添加执行权限
-chmod +x "$UCI_DEFAULTS"
+[ -f "$UCI_DEFAULTS" ] && chmod +x "$UCI_DEFAULTS"
